a1af7aad1d72e8243cea4d875b02a8a3c53b2fc5
Taylor Courage <taylorjamescourage@gmail.com>
2021-02-20 15:51:40 +0000

    Now pulls extensions from a file!

--- bot.py
+++ bot.py
@@ -2,16 +2,29 @@ import discord, sys, traceback, os
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-#Our list of extensions. Ideally this would be pulled from a file
-initial_extensions = ["modules.dt-module_kick",
-                        "modules.dt-module_ban",
-                        "modules.dt-module_roll"]
+#Our list of extensions - now pulled from files!
+with open('main-extensions.txt', 'r') as fd:
+    initial_extensions = fd.readlines()
+initial_extensions = [x.strip() for x in initial_extensions]
+
+with open('extra-extensions.txt', 'r') as fd:
+    extra_extensions = fd.readlines()
+extra_extensions = [x.strip() for x in extra_extensions]
+
 
 bot = commands.Bot(command_prefix='|')
 
 #Load our extensions
 if __name__ == "__main__":
     for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+            print(f"Loaded extention {extension}")
+        except Exception as e:
+            print(f"Failed to load extension {extension}.", file=sys.stderr)
+            traceback.print_exc()
+    
+    for extension in extra_extensions:
         try:
             bot.load_extension(extension)
             print(f"Loaded extention {extension}")
