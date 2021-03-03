a03bb40b7a4f0292ade1e46c82c4dee3d729f8a6
Taylor Courage <taylorjamescourage@gmail.com>
2021-03-03 17:12:48 -0500

    Large refactor of initial launcher - Now pulls token and command prefix from DB - First launch prompts user for the token and prefix - Moved some things around for better compatibilty

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@
 #                                                  #
 #                                                  #
 #                                                  #
-# Version 0.4.1                                    #
 ####################################################
 
 
@@ -21,10 +20,57 @@ import discord, sys, traceback, os, sqlite3
 from discord.ext import commands
 from discord.ext.commands import Bot
 
+print("|----------------------------------------------------------------------------------------|")
+print("| Welcome to Deep Thought! The modular, self-hosted, open-source Discord bot alternative |")
+print("| Please take your time to read about the project on GitHub, where you likely downloaded |")
+print("| this software, to learn more about its operation. Have fun!                            |")
+print("|----------------------------------------------------------------------------------------|\n")
+
 server_db = sqlite3.connect('users.db')  #Access our users database
 server_settings = sqlite3.connect('server.db')  #Access our server settings database
 c = server_db.cursor()
 s = server_settings.cursor()
+
+#Checks for databases, creates them if necessary
+
+print("Checking Server Settings Database status...\n")
+try:
+    s.execute('''SELECT * FROM server;''')
+    print("Using existing database\n----------------------------------------")
+except:
+    s.execute('''CREATE TABLE server(token text, prefix text);''') 
+
+try:
+    print ("Reading token file from DB...\n")
+    s.execute('''SELECT * FROM server;''')
+    row = s.fetchone()
+    token = row[0]
+    prefix = row[1]
+    print (f"Token found! Using. Server prefix is {prefix}\n----------------------------------------")
+except:
+    token = input("No token found! Please paste your bot token here: ")
+    prefix = input("Please select a command prefix. i.e. if you want your commands to be '!kick' you would enter '!'")
+    s.execute('''INSERT INTO server (token, prefix) VALUES (?,?);''', (token, prefix))
+    print ("Token and prefix successfully added to DB, will attempt login\n----------------------------------------")
+
+
+print("Checking User Database status...\n")
+try:
+    c.execute('''SELECT * FROM users;''')
+    print("Using existing database\n----------------------------------------")
+except:
+    print("Creating new DB, please run the 'setup' command on the server to populate.")
+    c.execute('''CREATE TABLE users(id int, name text, discriminator text, nick text, top_role text);''')
+    c.execute('''CREATE TABLE discipline(id int, strikes int, kicks int, banned int, bans int);''')
+
+server_db.commit()
+server_db.close()
+server_settings.commit()
+server_settings.close()
+
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
 
 #Our list of extensions - now pulled from files!
 try:
@@ -40,20 +86,10 @@ try:
 except FileNotFoundError:
     extra_extensions = []
 
-intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='|', intents=intents)
 
 #Load our extensions
 if __name__ == "__main__":
-    print("|---------------------------------------------|")
-    print("| Welcome to Deep Thought! The modular, self- |")
-    print("| hosted, open-source Discord bot alternative |")
-    print("| Please take your time to read about the pr- |")
-    print("| oject on GitHub, where you likely download- |")
-    print("| ed this software, to learn more about its   |")
-    print("| operation. Have fun!                        |")
-    print("|---------------------------------------------|")
-    print ("\nLoading main bot extensions...\n")
+    print ("Loading main bot extensions...\n")
     if initial_extensions:
         for extension in initial_extensions:
             try:
@@ -65,7 +101,7 @@ if __name__ == "__main__":
     else:
         print("No Main extensions found! Caution, may cause error. Skipping...")
         
-    print ("--------------------\nLoading extra bot extensions...\n")
+    print ("----------------------------------------\nLoading extra bot extensions...\n")
     if extra_extensions:
         for extension in extra_extensions:
             try:
@@ -81,26 +117,9 @@ if __name__ == "__main__":
 #What the bot runs when it's loaded and ready
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}\n--------------------\n'.format(bot))
+    print('We have logged in as {0.user}\n----------------------------------------\n'.format(bot))
 
-#Checks for databases, creates them if necessary
-print("--------------------\nChecking db status...\n")
-try:
-    c.execute('''SELECT * FROM users;''')
-    print("Using existing database")
-except:
-    print("Creating new DB, please run the 'setup' command on the server to populate.")
-    c.execute('''CREATE TABLE users(id int, name text, discriminator text, nick text, top_role text);''')
-    c.execute('''CREATE TABLE discipline(id int, strikes int, kicks int, banned int, bans int);''')
-    s.execute('''CREATE TABLE server(token text, prefix text);''')  # -- Commented out for now. Will revisit.
-server_db.commit()
-server_db.close()
-server_settings.commit()
-server_settings.close()
+print("----------------------------------------\nlogging in...\n")
 
-print("--------------------\nlogging in....\n")
-#This reads our token from the token file. This is ideal for security purposes when open-
-#sourcing a project like this and keeping modularity.
-with open ("token", "r") as tokenFile:
-    token = tokenFile.read()
+#Reads bot token from the database
 bot.run(token)
