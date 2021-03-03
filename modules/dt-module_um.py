40400cbcf8624b4480925e1b8d42e58efc5d22d0
Taylor Courage <taylorjamescourage@gmail.com>
2021-03-03 15:35:32 -0500

    Updated so warning messages tag users

--- modules/dt-module_um.py
+++ modules/dt-module_um.py
@@ -24,7 +24,7 @@ class umModule(commands.Cog):
         #Open our DB
         server_db = sqlite3.connect('server.db')
         c = server_db.cursor()
-        c.execute('''SELECT bans FROM discipline WHERE id = (?)''', (user_name.id,)) #Gets the number of times they've been kicked before
+        c.execute('''SELECT bans FROM discipline WHERE id = (?);''', (user_name.id,)) #Gets the number of times they've been kicked before
         num_bans = c.fetchone()  #fetches the SQL row
         bans = num_bans[0] #create a variable to hold the number of bans
         
@@ -37,12 +37,12 @@ class umModule(commands.Cog):
         elif reason != None: #If a reason is given
             bans += 1 #add to the kick counter
             c.execute('''UPDATE discipline SET bans = (?) WHERE id = (?);''', (bans, user_name.id,)) #apply kick count
-            await ctx.send(f"{user_name} is now banned, reason(s); {reason}")
+            await ctx.send(f"{user_name.mention} is now banned, reason(s); {reason}")
             await ctx.guild.ban(user_name)
         else: #If a reason is not given
             bans += 1 #add to the kick counter
             c.execute('''UPDATE discipline SET bans = (?) WHERE id = (?);''', (bans, user_name.id,)) #apply kick count
-            await ctx.send(f"{user_name} is now banned")
+            await ctx.send(f"{user_name.mention} is now banned")
             await ctx.guild.ban(user_name)
 
         #Write changes and close the DB
@@ -66,14 +66,14 @@ class umModule(commands.Cog):
         #Open our DB
         server_db = sqlite3.connect('server.db')
         c = server_db.cursor()
-        c.execute('''SELECT kicks FROM discipline WHERE id = (?)''', (user_name.id,)) #Gets the number of times they've been kicked before
+        c.execute('''SELECT kicks FROM discipline WHERE id = (?);''', (user_name.id,)) #Gets the number of times they've been kicked before
         num_kicks = c.fetchone()  #fetches the SQL row
         kicks = num_kicks[0] #create a variable to hold the number of kicks
 
         if reason != None: #If a reason is given
             kicks += 1 #add to the kick counter
             c.execute('''UPDATE discipline SET kicks = (?) WHERE id = (?);''', (kicks, user_name.id,)) #apply kick count
-            await ctx.send(f"{user_name} has been kicked, reason(s); {reason}")
+            await ctx.send(f"{user_name.mention} has been kicked, reason(s); {reason}")
             await ctx.guild.kick(user_name)
         else: #If a reason is not given
             kicks += 1
@@ -102,7 +102,7 @@ class umModule(commands.Cog):
         #Open our DB
         server_db = sqlite3.connect('server.db')
         c = server_db.cursor()
-        c.execute('''SELECT strikes, bans FROM discipline WHERE id = (?)''', (user_name.id,)) #Gets the number of strikes they've gotten before
+        c.execute('''SELECT strikes, bans FROM discipline WHERE id = (?);''', (user_name.id,)) #Gets the number of strikes they've gotten before
         num_strikes = c.fetchone()  #fetches the SQL row
         strikes = num_strikes[0] #create a variable to hold the number of strikes
         bans = num_strikes[1] #variable to hold number of bans
@@ -121,22 +121,22 @@ class umModule(commands.Cog):
             if strikes == 3:
                 bans += 1
                 c.execute('''UPDATE discipline SET strikes = 0, banned = 1, bans = (?) WHERE id = (?);''', (bans, user_name.id,))
-                await ctx.send(f"{user_name} has received three strikes, and is now banned. (Strike reason: {reason}")
+                await ctx.send(f"{user_name.mention} has received three strikes, and is now banned. (Strike reason: {reason}")
                 await ctx.guild.ban(user_name)
             else:
                 c.execute('''UPDATE discipline SET strikes = (?) WHERE id = (?);''', (strikes, user_name.id,)) #apply strike count
-                await ctx.send(f"{user_name} has been given a strike, reason(s); {reason}. This is strike #{strikes}")
+                await ctx.send(f"{user_name.mention} has been given a strike, reason(s); {reason}. This is strike #{strikes}")
 
         else: #If a reason is not given
             strikes += 1 #add to the strike counter
             if strikes == 3:
                 bans += 1
                 c.execute('''UPDATE discipline SET strikes = 0, banned = 1, bans = (?) WHERE id = (?);''', (bans, user_name.id,))
-                await ctx.send(f"{user_name} has received three strikes, and is now banned.")
+                await ctx.send(f"{user_name.mention} has received three strikes, and is now banned.")
                 await ctx.guild.ban(user_name)
             else:
                 c.execute('''UPDATE discipline SET strikes = (?) WHERE id = (?);''', (strikes, user_name.id,)) #apply strike count
-                await ctx.send(f"{user_name} has been given a strike. This is strike #{strikes}")
+                await ctx.send(f"{user_name.mention} has been given a strike. This is strike #{strikes}")
 
         #Write changes and close the DB
         server_db.commit()
