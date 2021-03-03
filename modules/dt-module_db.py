1e91cb251b13a4046dc2c8e55b15a20c5d739d0b
Taylor Courage <taylorjamescourage@gmail.com>
2021-03-03 14:27:30 -0500

    Lots of changes; - added DB to user manager - added strikes to user manager - added lots of comments

--- modules/dt-module_db.py
+++ modules/dt-module_db.py
@@ -2,73 +2,76 @@ import discord, sys, traceback, os, asyncio, sqlite3
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot, has_permissions, CheckFailure
 
-
+## So this is our database management module. It handles updating the database and keeping
+# it updated when new users join.
 
 class dbLaunch(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    #The update command. This handles when the user runs '|setup' or another alias and updates everything
     @commands.command(name="setup", aliases=["setupdb", "dbupdate", "update", "updatedb"], pass_context=True)
     @commands.has_permissions(administrator=True)
     async def setupdb(self, ctx):
         adds = 0
         changes = 0
-        #Open our DB, create if doesn't exist.
+        x = ctx.guild.members
 
+        #Open our DB
         server_db = sqlite3.connect('server.db')
         c = server_db.cursor()
+
         print("\n--------------------------------")
-        #try:
-        print("Writing users to database...\n")
-        print("Users added:\n")
-        x = ctx.guild.members
+        print("Updating database...\n")
+
         #If server perms are setup right, this grabs a list of every user on the server
         for member in x:
             #Create a list for the current users' info
             users = [member.id, member.name, member.discriminator, member.nick, member.top_role.name]
+            print(member)
             
             #This checks the database to see if the user already as an entry,
             #and skips adding them if they do.            
-            c.execute('''SELECT * FROM users WHERE id=?''', (member.id,))
+            c.execute('''SELECT * FROM users WHERE id = ?''', (member.id,))
             row = c.fetchone()
-            # If the user does not exist, we'll create them and add to the database
+            # If the user does not exist, we'll create them and add to the database;
             if row == None:
                 #If the member has no nickname, we'll set it as N/A
                 if member.nick == None:
-                    #try:
-                    users = [member.id, member.name, member.discriminator, 'N/A', member.top_role.name]
-                    c.execute('''INSERT INTO users (id, name, discriminator, nick, top_role) VALUES (?,?,?,?,?);''', users)
-                    c.execute('''INSERT INTO discipline (strikes, kicks, banned, bans) VALUES (?,?,?,?);''', [0, 0, 0, 0])
-                    print("User {} successfully added to the database;".format(users[1]))
-                    print(users)
-                    print("--------------------------------")
-                    adds += 1
-                    #except:
-                    #    print("Error inserting user {} in database".format(users[1]))
+                    try:
+                        users = [member.id, member.name, member.discriminator, 'N/A', member.top_role.name]
+                        c.execute('''INSERT INTO users (id, name, discriminator, nick, top_role) VALUES (?,?,?,?,?);''', users)
+                        c.execute('''INSERT INTO discipline (id, strikes, kicks, banned, bans) VALUES (?,?,?,?,?);''', [member.id, 0, 0, 0, 0])
+                        print("User {} successfully added to the database;".format(users[1]))
+                        print(users)
+                        print("--------------------------------")
+                        adds += 1
+                    except:
+                        print("Error inserting user {} in database".format(users[1]))
+                #If they have a nick, we'll set it
                 else:
-                    #try:
-                    c.execute('''INSERT INTO users (id, name, discriminator, nick, top_role) VALUES (?,?,?,?,?);''', users)
-                    c.execute('''INSERT INTO discipline (strikes, kicks, banned, bans) VALUES (?,?,?,?);''', [0, 0, 0, 0])
-                    print("User {} successfully added to the database;".format(users[1]))
-                    print(users)
-                    print("--------------------------------")
-                    adds += 1
-                    #except:
-                    #    print("Error inserting user {} in database".format(users[1]))
+                    try:
+                        c.execute('''INSERT INTO users (id, name, discriminator, nick, top_role) VALUES (?,?,?,?,?);''', users)
+                        c.execute('''INSERT INTO discipline (id, strikes, kicks, banned, bans) VALUES (?,?,?,?,?);''', [member.id, 0, 0, 0, 0])
+                        print("User {} successfully added to the database;".format(users[1]))
+                        print(users)
+                        print("--------------------------------")
+                        adds += 1
+                    except:
+                        print("Error inserting user {} in database".format(users[1]))
       
-
-
+            # Otherwise if the user *does* exist, we'll check if they need anything updated and do it.
             else:
-                if users[3] == row[3]:
+                if users[3] == row[3]: #If nothing is changed we skip
                     print("User {} already in database and no changes to be made, skipping...".format(users[1]))
-                elif users[3] != row[3] and users[3] != None:
+                elif users[3] != row[3] and users[3] != None:  #If they change their nick we'll update
                     try:
                         c.execute('''INSERT INTO users (nick) VALUES (?);''', users[3])
                         print("User {} updated successfully".format(users[1]))
                         changes += 1
                     except:
                         print("Error editing user {}".format(users[1]))
-                elif users[3] == None and row[3] != "N/A":
+                elif users[3] == None and row[3] != "N/A": #User removes nick we'll update that
                     print(row[3])
                     try:
                         c.execute('''INSERT INTO users (nick) VALUES ('N/A');''')
@@ -76,30 +79,43 @@ class dbLaunch(commands.Cog):
                         changes += 1
                     except:
                         print("Error editing user {}".format(users[1]))
+                elif users[1] != row[1] and users[1] != None: #If they change their actual Discord name (instead of server nick), we'll update that.
+                    try:
+                        c.execute('''INSERT INTO users (name) VALUES (?);''', users[1])
+                        print("User {} updated successfully".format(users[1]))
+                        changes += 1
+                    except:
+                        print("Error editing user {}".format(users[1]))
                 else:
                     print("User {} already in database and no changes to be made, skipping...".format(users[1]))
         print("--------------------------------")
+
+        #Lets the console user/server owner know if anything has been updated. Also sends results to the channel the update
+        #was requested in to give a quick and dirty update on what happened.
         if (changes > 0) or (adds > 0):
-            await ctx.send("Database updated with {} new and {} modified entries - please see server log for more details".format(adds, changes))
-            print("\nDatabase updated with {} new and {} modified entries - please see server log for more details".format(adds, changes))
+            await ctx.send("Database updated with {} new and {} modified entries - please see server log for more details.".format(adds, changes))
+            print("\nDatabase updated with {} new and {} modified entries - please see server log for more details.".format(adds, changes))
         else:
             await ctx.send("No changes were made to the database - up to date!")
             print("\nNo changes were made to the database - up to date!")
         print("\n--------------------------------\n")
 
+        #Write changes and close the DB
         server_db.commit()
         server_db.close()
 
+    #If a non-admin uses this command, tell the user they're not allowed to do that
     @setupdb.error
     async def db_perms_error(self, ctx, error):
         if isinstance(error, CheckFailure):
             messageAuthor = ctx.author.mention
-            await ctx.send(f"Sorry {messageAuthor}, you are not allowed to do that. Only administrators may update the database")
+            await ctx.send(f"Sorry {messageAuthor}, you are not allowed to do that. Only administrators may update the database.")
 
+    #This code automatically updates the database when new users join.
     @commands.Cog.listener("on_member_join")
     async def member_join_update(self, member: discord.Member):
         channel = member.guild.system_channel
-        await channel.send(f'Member {member.mention} has joined!')
+        #await channel.send(f'Member {member.mention} has joined!')
         print("New user {} has joined! Updating database...\n".format(member))
         adds = 0
         changes = 0
@@ -122,11 +138,11 @@ class dbLaunch(commands.Cog):
             # If the user does not exist, we'll create them and add to the database
             if row == None:
                 #If the member has no nickname, we'll set it as N/A
-                if member.nick == None:
+                if member.nick == None: #This is the same code as above, so just read the above to understand this better.
                     try:
                         users = [member.id, member.name, member.discriminator, 'N/A', member.top_role.name]
                         c.execute('''INSERT INTO users (id, name, discriminator, nick, top_role) VALUES (?,?,?,?,?);''', users)
-                        c.execute('''INSERT INTO discipline (strikes, kicks, banned, bans) VALUES (?,?,?,?);''', [0, 0, 0, 0])
+                        c.execute('''INSERT INTO discipline (id, strikes, kicks, banned, bans) VALUES (?,?,?,?,?);''', [member.id, 0, 0, 0, 0])
                         print("User {} successfully added to the database;".format(users[1]))
                         print(users)
                         print("--------------------------------")
@@ -136,7 +152,7 @@ class dbLaunch(commands.Cog):
                 else:
                     try:
                         c.execute('''INSERT INTO users (id, name, discriminator, nick, top_role) VALUES (?,?,?,?,?);''', users)
-                        c.execute('''INSERT INTO discipline (strikes, kicks, banned, bans) VALUES (?,?,?,?);''', [0, 0, 0, 0])
+                        c.execute('''INSERT INTO discipline (id, strikes, kicks, banned, bans) VALUES (?,?,?,?,?);''', [member.id, 0, 0, 0, 0])
                         print("User {} successfully added to the database;".format(users[1]))
                         print(users)
                         print("--------------------------------")
@@ -147,16 +163,16 @@ class dbLaunch(commands.Cog):
 
 
             else:
-                if users[3] == row[3]:
+                if users[3] == row[3]: #If the user exists, we skip them
                     print("User {} already in database and no changes to be made, skipping...".format(users[1]))
-                elif users[3] != row[3] and users[3] != None:
+                elif users[3] != row[3] and users[3] != None: #If the new nickname doesn't match the old one, we'll update it
                     try:
                         c.execute('''INSERT INTO users (nick) VALUES (?);''', users[3])
                         print("User {} updated successfully".format(users[1]))
                         changes += 1
                     except:
                         print("Error editing user {}".format(users[1]))
-                elif users[3] == None and row[3] != "N/A":
+                elif users[3] == None and row[3] != "N/A": #If they remove their nickname, update
                     print(row[3])
                     try:
                         c.execute('''INSERT INTO users (nick) VALUES ('N/A');''')
@@ -164,7 +180,7 @@ class dbLaunch(commands.Cog):
                         changes += 1
                     except:
                         print("Error editing user {}".format(users[1]))
-                elif users[1] != row[1] and users[1] != None:
+                elif users[1] != row[1] and users[1] != None: #If they change their actual Discord name (instead of server nick), we'll update that.
                     try:
                         c.execute('''INSERT INTO users (name) VALUES (?);''', users[1])
                         print("User {} updated successfully".format(users[1]))
@@ -182,9 +198,10 @@ class dbLaunch(commands.Cog):
             print("\nNo changes were made to the database - up to date!")
         print("\n--------------------------------\n")
 
+        # Write to and close the DB
         server_db.commit()
         server_db.close()
 
 
-def setup(bot):
+def setup(bot): #setup and launch bot
     bot.add_cog(dbLaunch(bot))
--- modules/dt-module_um.py
+++ modules/dt-module_um.py
@@ -7,7 +7,7 @@
 #####################################
 
 
-import discord
+import discord, string, sqlite3
 from discord import User
 from discord.ext import commands
 from discord.ext.commands import Bot, has_permissions, CheckFailure
@@ -17,22 +17,40 @@ class umModule(commands.Cog):
         self.bot = bot
 
     #Ban module. Code is pretty self-explanitory
-    @commands.command(pass_context = True)
-    @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, user_name: discord.Member, days = 1, reason = "reasons"):
-        if user_name == None or user_name == ctx.message.author:
+    @commands.command(nam = "ban", pass_context = True)
+    @commands.has_permissions(ban_members=True) #This ensures only people who are allowed to ban others can use this command
+    async def ban(self, ctx, user_name: discord.Member, reason = None):
+
+        #Open our DB
+        server_db = sqlite3.connect('server.db')
+        c = server_db.cursor()
+        c.execute('''SELECT bans FROM discipline WHERE id = (?)''', (user_name.id,)) #Gets the number of times they've been kicked before
+        num_bans = c.fetchone()  #fetches the SQL row
+        bans = num_bans[0] #create a variable to hold the number of bans
+        
+        if user_name == ctx.message.author:  #Prevent user from banning themself
             await ctx.send("You cannot ban yourself")
             return
-        try:
-            await ctx.guild.ban(user_name, reason = reason)
-            if days > 1:
-                await ctx.send(f"{user_name} is now banned for {days} days for {reason}")
-            else:
-                await ctx.send(f"{user_name} is now banned for {days} day for {reason}")
-        except discord.Forbidden:
-            messageAuthor = ctx.author.mention
-            await ctx.send(f"Sorry {messageAuthor}, you are not allowed to do that")
+        elif user_name == None: #Forces you to tag someone
+            await ctx.send("Please tag someone to ban")
+            return
+        elif reason != None: #If a reason is given
+            bans += 1 #add to the kick counter
+            c.execute('''UPDATE discipline SET bans = (?) WHERE id = (?);''', (bans, user_name.id,)) #apply kick count
+            await ctx.send(f"{user_name} is now banned, reason(s); {reason}")
+            await ctx.guild.ban(user_name)
+        else: #If a reason is not given
+            bans += 1 #add to the kick counter
+            c.execute('''UPDATE discipline SET bans = (?) WHERE id = (?);''', (bans, user_name.id,)) #apply kick count
+            await ctx.send(f"{user_name} is now banned")
+            await ctx.guild.ban(user_name)
 
+        #Write changes and close the DB
+        server_db.commit()
+        server_db.close()
+
+
+    #If the user doesn't have the permissions to ban people, they're told they can't
     @ban.error
     async def ban_error(self, ctx, error):
         if isinstance(error, CheckFailure):
@@ -43,10 +61,31 @@ class umModule(commands.Cog):
     #Kick module. Also pretty self-explanitory
     @commands.command(name="kick", aliases=["boot", "toss"], pass_context = True)
     @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx, user_name: discord.Member, reason = "reasons unknown (Check #mod-actions for details)"):
-        await ctx.guild.kick(user_name, reason = reason)
-        await ctx.send(f"{user_name} was kicked for {reason}")
+    async def kick(self, ctx, user_name: discord.Member, reason = None):
+
+        #Open our DB
+        server_db = sqlite3.connect('server.db')
+        c = server_db.cursor()
+        c.execute('''SELECT kicks FROM discipline WHERE id = (?)''', (user_name.id,)) #Gets the number of times they've been kicked before
+        num_kicks = c.fetchone()  #fetches the SQL row
+        kicks = num_kicks[0] #create a variable to hold the number of kicks
+
+        if reason != None: #If a reason is given
+            kicks += 1 #add to the kick counter
+            c.execute('''UPDATE discipline SET kicks = (?) WHERE id = (?);''', (kicks, user_name.id,)) #apply kick count
+            await ctx.send(f"{user_name} has been kicked, reason(s); {reason}")
+            await ctx.guild.kick(user_name)
+        else: #If a reason is not given
+            kicks += 1
+            c.execute('''UPDATE discipline SET kicks = (?) WHERE id = (?);''', (kicks, user_name.id,))
+            await ctx.send(f"{user_name} has been kicked")
+            await ctx.guild.kick(user_name)
+
+        #Write changes and close the DB
+        server_db.commit()
+        server_db.close()
     
+    #Similar to the ban error above
     @kick.error
     async def kick_error(self, ctx, error):
         if isinstance(error, CheckFailure):
@@ -54,19 +93,62 @@ class umModule(commands.Cog):
             await ctx.send(f"Sorry {messageAuthor}, you are not allowed to do that")
 
 
-#####    UNBAN CURRENTLY BROKEN -- DOES NOT WORK   #####
-#####  Likely will require SQL Server to function  #####
-'''
-    @commands.command(pass_context = True)
-    async def unban(self, ctx, id: int):
-        userID = bot.get_user(id)
-        try:
-            await ctx.guild.unban(userID)
-            await ctx.send(f"{user_name} is now unbanned")
-        except discord.Forbidden:
+
+    #Strike module. Gives strikes. Can only be used by people who are allowed to ban
+    @commands.command(name = "strike", aliases = ["warn", "warning"], pass_context = True)
+    @commands.has_permissions(ban_members=True) #This ensures only people who are allowed to ban others can use this command
+    async def strike(self, ctx, user_name: discord.Member, reason = None):
+
+        #Open our DB
+        server_db = sqlite3.connect('server.db')
+        c = server_db.cursor()
+        c.execute('''SELECT strikes, bans FROM discipline WHERE id = (?)''', (user_name.id,)) #Gets the number of strikes they've gotten before
+        num_strikes = c.fetchone()  #fetches the SQL row
+        strikes = num_strikes[0] #create a variable to hold the number of strikes
+        bans = num_strikes[1] #variable to hold number of bans
+        print (user_name.id)
+        print (strikes)
+        print (bans)
+        
+        if user_name == ctx.message.author:  #Prevent user from giving themself strikes
+            await ctx.send("You cannot give yourself strikes")
+            return
+        elif user_name == None: #Forces you to tag someone
+            await ctx.send("Please tag someone to give them a strike")
+            return
+        elif reason != None: #If a reason is given
+            strikes += 1 #add to the strike counter
+            if strikes == 3:
+                bans += 1
+                c.execute('''UPDATE discipline SET strikes = 0, banned = 1, bans = (?) WHERE id = (?);''', (bans, user_name.id,))
+                await ctx.send(f"{user_name} has received three strikes, and is now banned. (Strike reason: {reason}")
+                await ctx.guild.ban(user_name)
+            else:
+                c.execute('''UPDATE discipline SET strikes = (?) WHERE id = (?);''', (strikes, user_name.id,)) #apply strike count
+                await ctx.send(f"{user_name} has been given a strike, reason(s); {reason}. This is strike #{strikes}")
+
+        else: #If a reason is not given
+            strikes += 1 #add to the strike counter
+            if strikes == 3:
+                bans += 1
+                c.execute('''UPDATE discipline SET strikes = 0, banned = 1, bans = (?) WHERE id = (?);''', (bans, user_name.id,))
+                await ctx.send(f"{user_name} has received three strikes, and is now banned.")
+                await ctx.guild.ban(user_name)
+            else:
+                c.execute('''UPDATE discipline SET strikes = (?) WHERE id = (?);''', (strikes, user_name.id,)) #apply strike count
+                await ctx.send(f"{user_name} has been given a strike. This is strike #{strikes}")
+
+        #Write changes and close the DB
+        server_db.commit()
+        server_db.close()
+
+
+    #If the user doesn't have the permissions to ban people, they're told they can't give strikes either
+    @strike.error
+    async def strike_error(self, ctx, error):
+        if isinstance(error, CheckFailure):
             messageAuthor = ctx.author.mention
             await ctx.send(f"Sorry {messageAuthor}, you are not allowed to do that")
-'''
 
 
 def setup(bot):
